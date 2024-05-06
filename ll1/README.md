1112f293954c4f9097d34b0d64c10c45d9e07e5b
shoshtari <maleki@gmail.com>
2024-05-07 01:30:11 +0330

    feat: completed ll1 transformer

--- ll1/README.md
+++ ll1/README.md
@@ -1,8 +1,46 @@
 # LL1 converter 
 this project tries to convert a grammar into LL1 form.
+## eliminate_left_recursion
 first step is to remove the left recursion from the grammar.
 we do this by using the formula specified in comments. 
+```
+A -> AX | Y
+convert to:
+A -> YA'
+A' -> XA' | ε
+```
 we use extra non terminals to achieve this.
-then we want to remove common prefix to make our grammar deterministic. 
-we do it by using a simple idea! assume that `a` is in the first set of `A`. so we know there is some transition like `A -> a X` where `X` can be some non terminals. so in each step we try to make non terminals of grammar deterministic. but by doing this, we make some new undeterministic non terminals. but hopefully their count is lower than the original grammar. 
-so we keep simplifying grammar till we add no more non terminals and then we return the grammar.
+## first and follow
+we define first(A) as a set of first terminals we can derviate from A (A is non terminal)
+we define follow(A) as all y's that we have `S =>* AyX`.
+there is a visited set to avoid infinite loops. because we use recursive call to find first and follow set.
+## find_follow
+now assume `x` is in `first(A)`. so we know that we can have a rule like `A -> xX` where `X` can be a set of non terminals and terminals. 
+so what `find follow` does is to find all possible values for `X`. so later we can simplify grammar by these formula:
+```
+A -> aB | aC | aD | bc | E (a is not in first(E))
+converts to (note that find_follow(A, a) = {B, C, D}):
+A -> aX | bc | E
+X -> B, C, D
+```
+## get_trouble_nts
+it iterate over all non terminals of grammar and check if we have undeterminicity for a non terminal and return all non terminals that are not deterministic. if there isn't any, then our grammar is ll1. else we try to simplify them using `find_follow`.
+## remove_left_common
+it tries to simplify grammar by making all non terminals deterministic. 
+so it has a recursive call until the output of `get_trouble_nts` is empty. 
+## remove dummy non terminals
+the grammar my generate some rules like `A -> []` in some corner cases.
+it doesn't make grammar incorrect but it is a bit lame to show products like this. more than this, since we add more non terminals to make our grammar ll1, it is important that we don't waste any non terminal.
+
+so this function tries to remove dummy and unit rules to free some non terminals.
+## convert to ll1 
+it just calls `eliminate_left_recursion` and `remove_left_common` on the grammar.
+## simplize 
+since we may add many more non terminals, some of them might be unneccessary. so by calling simplize we try to make grammar more consise and avoid any unneccessary states.
+it
+it does its work by making `reach` and `term` set and then just keep non terminals that are both reachable and terminable.
+it doesn't chage a grammar to make it more like ll1 or less, it just helps with time complexity and also output a nicer grammar.
+
+## final 
+by calling `convert to ll1 ` and the `simplize` we can have nice compact ll1 grammars. 
+the only danger is that since it has a maximum non terminal count as 28, if we need more non terminals, it cannot convert it to ll1 grammar.
--- ll1/grammer.py
+++ ll1/grammer.py
@@ -66,12 +66,11 @@ def first(grammer, symbol, visited = None):
                 res = first(grammer, char)
                 ans = ans.union(res)
                 ans.remove("")
-            if p_ind == len(product):
+            if "" in res and p_ind == len(product):
                 ans.add("")
-
     return ans 
 
-def follow(grammar, symbol, visited = set()):
+def follow(grammar, symbol):
     """
     we must return all b's that we have in a form in: s->* XAbC for follow(A)
     """
@@ -150,6 +149,7 @@ def remove_dummy_non_terminals(grammar):
     for nt in grammar:
         if len(grammar[nt]) == 1 and '' in grammar[nt]:
             dummies.add(nt)
+
     new_grammar = {}
     for nt in grammar:
         if nt in dummies:
@@ -161,23 +161,45 @@ def remove_dummy_non_terminals(grammar):
                 new_p = new_p.replace(d, '')
             new_grammar[nt].append(new_p)
     return new_grammar
-def remove_left_common(grammar):
+
+def get_trouble_nts(grammar):
+    ans = set()
+    for nt in grammar:
+        if not all([len(p) == 0 or is_terminal(p[0]) for p in grammar[nt]]):
+            ans.add(nt)
+    return list(ans)
+
+def remove_left_common(grammar, chars = None):
     available_non_terminals = list(set(alphabet.upper()) - set(grammar.keys()))
     new_grammer = {}
 
+    if chars is None:
+        chars = list(grammar.keys())
+
     for nt in grammar.keys():
+        if nt not in chars:
+            new_grammer[nt] = grammar[nt].copy()
+            continue
+
         try:
             new_nt = available_non_terminals.pop()
         except:
             return grammar
         
+        new_grammer[nt] = []
         for char in first(grammar, nt):
-            new_grammer[nt] = [f"{char}{new_nt}"]
-            new_grammer[new_nt] = list(find_follow(grammar, nt, char))
+            new_product = list(find_follow(grammar, nt, char))
+            if len(new_product) == 0:
+                new_grammer[nt].append(char)
+            else:
+                new_grammer[nt].append(f"{char}{new_nt}")
+                new_grammer[new_nt] = new_product
     new_grammer = remove_dummy_non_terminals(new_grammer)
-    # if len(new_grammer) != len(grammar):
-    #     new_grammer = remove_left_common(new_grammer)
-    return new_grammer
+    troubles = get_trouble_nts(new_grammer)
+    if len(troubles) == 0:
+        return new_grammer
+    return remove_left_common(new_grammer, chars = troubles)
+
             
 
 def convert_to_ll1(grammar):
@@ -185,6 +207,45 @@ def convert_to_ll1(grammar):
     grammar = remove_left_common(grammar)
     return grammar
 
+def simplize(grammar, start = 'S'):
+    reach = set() 
+    term = set()
+
+    # reach
+    reach.add(start)
+    products = grammar[start].copy()
+    while products:
+        product = products.pop()
+        for char in product:
+            if not is_terminal(char):
+                reach.add(char)
+    
+    # term 
+    is_all_terminal = lambda s: all([is_terminal(c) or c in term for c in s])
+    products = []
+    for nt in grammar:
+        for product in grammar[nt]:
+            if is_all_terminal(product):
+                term.add(nt)
+    last_term = None 
+    while last_term != term:
+        last_term = term.copy()
+        for nt in grammar:
+            for product in grammar[nt]:
+                if is_all_terminal(product):
+                    term.add(nt)
+
+    # pruning grammar 
+    new_grammar = {}
+    for nt in grammar:
+        if nt not in reach or nt not in term:
+            continue
+        new_grammar[nt] = []
+        for p in grammar[nt]:
+            if is_all_terminal(p):
+                new_grammar[nt].append(p)
+    return new_grammar
+    
 # Example input grammar
 grammars = [
 {
@@ -197,6 +258,7 @@ grammars = [
 
 for i in range(len(grammars)):
     g = convert_to_ll1(grammars[i])
+    g = simplize(g)
     for k in  g:
         print(f"for non terminal {k} first set is {first(g, k)} and follow set is {follow(g, k)}")
     print(g)
